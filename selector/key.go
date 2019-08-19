779e3e8c62cfc896a42f668b6a42e6682233e5ea
Brooke Bryan <brooke@bajb.net>
2019-08-19 22:21:12 +0100

    Predicate & Request Keys

--- selector/key.go
+++ selector/key.go
@@ -6,3 +6,31 @@ const (
 	KeyNow           Key = "now"
 	KeyRandomPercent Key = "randompercent"
 )
+
+type RequestKey Key
+
+const (
+	RequestKeyType             RequestKey = "type"
+	RequestKeyLabel            RequestKey = "label"
+	RequestKeyContract         RequestKey = "contract"
+	RequestKeyScheduleAttempt  RequestKey = "scheduleattempt"
+	RequestKeyFailedAttempts   RequestKey = "failedattempts"
+	RequestKeyRenewalNumber    RequestKey = "renewalnumber"
+	RequestKeyAmount           RequestKey = "amount"
+	RequestKeyCurrency         RequestKey = "currency"
+	RequestKeyTaxAmount        RequestKey = "taxamount"
+	RequestKeyTaxCurrency      RequestKey = "taxcurrency"
+	RequestKeyDeliveryAmount   RequestKey = "deliveryamount"
+	RequestKeyDeliveryCurrency RequestKey = "deliverycurrency"
+	RequestKeyProductType      RequestKey = "producttype"
+	RequestKeySkuType          RequestKey = "skutype"
+	RequestKeySkuCode          RequestKey = "skucode"
+	RequestKeyDeliveryType     RequestKey = "deliverytype"
+	RequestKeyDeliveryStandard RequestKey = "deliverystandard"
+	RequestKeyCountry          RequestKey = "country"
+	RequestKeyRegion           RequestKey = "region"
+	RequestKeyPeriod           RequestKey = "period"
+	RequestKeyInitiated        RequestKey = "initiated"
+	RequestKeyExpiry           RequestKey = "expiry"
+	RequestKeyLastAttempted    RequestKey = "lastattempted"
+)
