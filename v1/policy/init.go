cdebca4fadf77a8a03a9356ddef44c157dd05f72
Brooke Bryan <brooke@bajb.net>
2019-08-20 16:38:39 +0100

    Init Fraud Policies

--- v1/policy/init.go
+++ v1/policy/init.go
@@ -4,10 +4,17 @@ import "github.com/chargehive/configuration/object"
 
 func AddHandlers() {
 	scaPolicy()
+	fraudPolicy()
 }
 
 func scaPolicy() {
 	o := ScaPolicy{}
 	object.AddKindHandler(o.GetKind(), object.KindHandlerDefaultVersion, func() object.Specification { return &ScaPolicy{} })
 	object.AddKindHandler(o.GetKind(), o.GetVersion(), func() object.Specification { return &ScaPolicy{} })
+}
+
+func fraudPolicy() {
+	o := FraudPolicy{}
+	object.AddKindHandler(o.GetKind(), object.KindHandlerDefaultVersion, func() object.Specification { return &FraudPolicy{} })
+	object.AddKindHandler(o.GetKind(), o.GetVersion(), func() object.Specification { return &FraudPolicy{} })
 }
