5804dd0dd8e0a8fd026e97f287872d356b6651af
Brooke Bryan <brooke@bajb.net>
2019-08-20 11:03:23 +0100

    Fraud Policy

---
+++ v1/policy/fraud.go
@@ -0,0 +1,47 @@
+package policy
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/chargehive/configuration/object"
+)
+
+const KindPolicyFraud object.Kind = "PolicyFraud"
+
+type FraudCheckType string
+
+const (
+	FraudCheckTypeAll      FraudCheckType = "all"      //Perform a fraud check on all provided connectors
+	FraudCheckTypeFailover FraudCheckType = "failover" //Perform a fraud check on one connector ID at a time, stopping at the first success
+)
+
+type FraudCheckTime string
+
+const (
+	FraudCheckTimePreFirstAuth FraudCheckTime = "preauth-first" //Run fraud check before the first auth
+	FraudCheckTimePreEveryAuth FraudCheckTime = "preauth-every" //Run fraud check before every auth
+	FraudCheckTimeAuthSuccess  FraudCheckTime = "auth-success"  //Run after a successful auth
+	FraudCheckTimeOnDemand     FraudCheckTime = "ondemand"      //Run on demand only
+)
+
+type FraudPolicy struct {
+	ConnectorIDs []string
+	CheckTime    FraudCheckTime
+	CheckType    FraudCheckType
+}
+
+func (FraudPolicy) GetKind() object.Kind { return KindPolicyFraud }
+func (FraudPolicy) GetVersion() string   { return "v1" }
+
+func NewFraudDefinition(d *object.Definition) (*FraudDefinition, error) {
+	if _, ok := d.Spec.(*FraudPolicy); ok {
+		return &FraudDefinition{def: d}, nil
+	}
+	return nil, errors.New("invalid fraud policy object")
+}
+
+type FraudDefinition struct{ def *object.Definition }
+
+func (d *FraudDefinition) Definition() *object.Definition { return d.def }
+func (d *FraudDefinition) MarshalJSON() ([]byte, error)   { return json.Marshal(d.def) }
+func (d *FraudDefinition) Spec() *FraudPolicy             { return d.def.Spec.(*FraudPolicy) }
--- v1/policy/sca.go
+++ v1/policy/sca.go
@@ -11,29 +11,29 @@ const KindPolicySCA object.Kind = "PolicySCA"
 type SCABypassMode string
 
 const (
+	SCABypassModeNone    SCABypassMode = ""        //Do not bypass a required challenge
 	SCABypassModeCascade SCABypassMode = "cascade" //Attempt the auth on the next connector
 	SCABypassModeCurrent SCABypassMode = "current" //Bypass, but stay on the current connector
 )
 
 type ScaPolicy struct {
 	ShouldIdentify          bool
-	ShouldChallengeOptional bool
-	ShouldByPassChallenge   bool
-	ChallengeByPassMode     SCABypassMode
+	ShouldChallengeOptional bool          //If the challenge is optional, setting this to false will not display the challenge
+	ShouldByPassChallenge   SCABypassMode //If the challenge is required, bypassing this will attempt an auth without displaying the challenge
 }
 
 func (ScaPolicy) GetKind() object.Kind { return KindPolicySCA }
 func (ScaPolicy) GetVersion() string   { return "v1" }
 
-func NewScaDefinition(d *object.Definition) (*Definition, error) {
+func NewScaDefinition(d *object.Definition) (*ScaDefinition, error) {
 	if _, ok := d.Spec.(*ScaPolicy); ok {
-		return &Definition{def: d}, nil
+		return &ScaDefinition{def: d}, nil
 	}
 	return nil, errors.New("invalid sca policy object")
 }
 
-type Definition struct{ def *object.Definition }
+type ScaDefinition struct{ def *object.Definition }
 
-func (d *Definition) Definition() *object.Definition { return d.def }
-func (d *Definition) MarshalJSON() ([]byte, error)   { return json.Marshal(d.def) }
-func (d *Definition) Spec() *ScaPolicy               { return d.def.Spec.(*ScaPolicy) }
+func (d *ScaDefinition) Definition() *object.Definition { return d.def }
+func (d *ScaDefinition) MarshalJSON() ([]byte, error)   { return json.Marshal(d.def) }
+func (d *ScaDefinition) Spec() *ScaPolicy               { return d.def.Spec.(*ScaPolicy) }
