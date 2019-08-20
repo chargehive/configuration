2ddc30386847664bae70268000f6e36d118f43c0
Brooke Bryan <brooke@bajb.net>
2019-08-20 16:40:09 +0100

    Init policies

--- v1/version.go
+++ v1/version.go
@@ -2,10 +2,12 @@ package v1
 
 import (
 	"github.com/chargehive/configuration/v1/connector"
+	"github.com/chargehive/configuration/v1/policy"
 	"github.com/chargehive/configuration/v1/scheduler"
 )
 
 func AddHandlers() {
 	connector.AddHandlers()
+	policy.AddHandlers()
 	scheduler.AddHandlers()
 }
