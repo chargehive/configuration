188074b2bb0d434b09e7eaba062883cf83b558ec
Brooke Bryan <brooke@bajb.net>
2019-09-04 22:54:34 +0100

    Selector config

--- cmd/generate.go
+++ cmd/generate.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"github.com/LucidCube/chargehive-transport-config/connectorconfig"
 	"github.com/chargehive/configuration/object"
+	"github.com/chargehive/configuration/selector"
+	"github.com/chargehive/configuration/v1/scheduler"
 	"io/ioutil"
 	"log"
 	"os"
@@ -17,11 +18,17 @@ var id = flag.String("id", "test-spec", "Spec ID")
 var projectID = flag.String("project", "", "Project ID")
 
 func buildSpec() object.Specification {
-	creds := connectorconfig.SandboxCredentials{
-		Mode:                connectorconfig.SandboxModeDynamic,
-		TransactionIDPrefix: "staging-",
-	}
-	return creds.ToConnector()
+	return scheduler.Initiator{Type: scheduler.InitiatorTypeAuth, InitialConnector: scheduler.ConnectorSelectorConfig,
+		AttemptConfig: &scheduler.AttemptConfig{
+			PoolType:                 scheduler.PoolTypeCascade,
+			MethodSelector:           scheduler.MethodSelectorPrimaryMethod,
+			OverridePoolConnectorIDs: []string{"paysafe-connector"}}}
+}
+
+func buildSelector() selector.Selector {
+	return selector.Selector{Priority: 50, Expressions: []selector.Predicate{
+		{Key: string(selector.RequestKeyCurrency), Operator: selector.PredicateOperatorEqual, Conversion: selector.OperatorConversionDefault, Values: []string{"GBP"}},
+	}}
 }
 
 func main() {
@@ -33,6 +40,7 @@ func main() {
 	def := object.DefinitionFromSpec(spec)
 	def.MetaData.Name = object.CleanName(*id)
 	def.MetaData.ProjectID = *projectID
+	def.Selector = buildSelector()
 
 	data, err := json.Marshal(def)
 
