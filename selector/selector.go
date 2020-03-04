25d6c8608383b014f28f5ca6d33ee8e9e0006349
Steve Barrow <steve@infurio.com>
2020-03-04 20:58:30 +0000

    set minimum selector priority

--- selector/selector.go
+++ selector/selector.go
@@ -1,6 +1,6 @@
 package selector
 
 type Selector struct {
-	Priority    int32       `json:"priority" yaml:"priority"`
+	Priority    int32       `json:"priority" yaml:"priority" validate:"gte=0"`
 	Expressions []Predicate `json:"expressions" yaml:"expressions" validate:"dive"`
 }
