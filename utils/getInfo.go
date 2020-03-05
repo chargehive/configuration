d5cc1299c7c8fc60c9bb6139565569712c829b0f
Steve Barrow <steve@infurio.com>
2020-03-05 14:22:11 +0000

    get info fixes and updates to unit tests

--- utils/getInfo.go
+++ utils/getInfo.go
@@ -1,34 +1,19 @@
 package utils
 
 import (
-	"errors"
+	"encoding/json"
 	"github.com/chargehive/configuration/object"
 )
 
 type ConfigInfo struct {
-	Kind        string
-	Version     string
-	Name        string
-	ProjectId   string
-	Disabled    bool
-	DisplayName string
+	Kind        object.Kind
+	MetaData    object.MetaData
+	SpecVersion string
 }
 
 // Takes a json config, deserializes and returns summary config info
-func GetInfo(rawJson []byte, version string) (ConfigInfo, error) {
-	if version != "v1" {
-		return ConfigInfo{}, errors.New("invalid config version")
-	}
-	def, err := object.FromJson(rawJson)
-	if err != nil {
-		return ConfigInfo{}, err
-	}
-	return ConfigInfo{
-		Kind:        string(def.Kind),
-		Version:     def.SpecVersion,
-		Name:        def.MetaData.Name,
-		ProjectId:   def.MetaData.ProjectID,
-		Disabled:    def.MetaData.Disabled,
-		DisplayName: def.MetaData.DisplayName,
-	}, nil
+func GetInfo(rawJson []byte) (ConfigInfo, error) {
+	obj := ConfigInfo{}
+	err := json.Unmarshal(rawJson, &obj)
+	return obj, err
 }
--- utils/getInfo_test.go
+++ utils/getInfo_test.go
@@ -2,22 +2,28 @@ package utils
 
 import (
 	"github.com/chargehive/configuration"
+	"github.com/chargehive/configuration/object"
 	"github.com/go-playground/assert/v2"
 	"testing"
 )
 
 // test for missing field
 func TestGetInfo(t *testing.T) {
-	rawJson := []byte(`{"kind":"Connector","metadata":{"projectId":"CHANGE-ME","name":"CHANGE-ME","displayName":"gary","description":"","annotations":null,"labels":null,"disabled":true},"specVersion":"v1","selector":{"priority":50,"expressions":[{"key":"charge.amount.currency","operator":"Equal","conversion":"","values":["GBP"]}]},"spec":{"library":"sandbox","configuration":"eyJtb2RlIjoiZHluYW1pYyIsInlyYW5zYWN0aW9uSURQcmVmaXgiOiIifQ=="}}`)
+	rawJson := []byte(`{"kind":"Project","metadata":{"projectId":"CHANGE-ME","name":"CHANGE-ME","displayName":"gary","description":"","annotations":null,"labels":null,"disabled":true},"specVersion":"v1","selector":{"priority":50,"expressions":[{"key":"charge.amount.currency","operator":"Equal","conversion":"","values":["GBP"]}]},"spec":{"library":"sandbox","configuration":"eyJtb2RlIjoiZHluYW1pYyIsInlyYW5zYWN0aW9uSURQcmVmaXgiOiIifQ=="}}`)
 	configuration.Initialise()
-	info, err := GetInfo(rawJson, "v1")
+	info, err := GetInfo(rawJson)
 	assert.Equal(t, err, nil)
 	assert.Equal(t, info, ConfigInfo{
-		Kind:        "Connector",
-		Version:     "v1",
-		Name:        "CHANGE-ME",
-		ProjectId:   "CHANGE-ME",
-		Disabled:    true,
-		DisplayName: "gary",
+		Kind: "Project",
+		MetaData: object.MetaData{
+			ProjectID:   "CHANGE-ME",
+			Name:        "CHANGE-ME",
+			DisplayName: "gary",
+			Description: "",
+			Annotations: nil,
+			Labels:      nil,
+			Disabled:    true,
+		},
+		SpecVersion: "v1",
 	})
 }
