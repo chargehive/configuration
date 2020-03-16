5567ee583ac063db77f264013cd187b42ea04c1d
Luke Hines <lukehines@protonmail.com>
2020-03-16 10:52:34 +0000

    Adds duration input test

--- object/duration_input.go
+++ object/duration_input.go
@@ -10,11 +10,10 @@ type DurationInput int64
 
 // GetSeconds returns the seconds value of the duration input
 func (d DurationInput) GetSeconds() int64 {
-	intVal := int64(d)
-	if intVal != 0 && time.Duration(intVal) > ((time.Hour*24)*3650) {
-		return intVal / int64(time.Second)
+	if time.Duration(d) < time.Second {
+		return int64(d)
 	}
-	return int64(d)
+	return int64(d) / int64(time.Second)
 }
 
 // GetDuration returns a time.Duration for the user input
@@ -32,6 +31,6 @@ func (d DurationInput) GetRawValue() int64 {
 }
 
 // MarshalJSON always returns seconds
-func (d *DurationInput) MarshalJSON() ([]byte, error) {
-	return json.Marshal(d.GetSeconds)
+func (d DurationInput) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.GetSeconds())
 }
---
+++ object/duration_input_test.go
@@ -0,0 +1,63 @@
+package object
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+)
+
+var (
+	testSeconds  int64 = 120
+	testDuration       = int64(time.Second) * testSeconds
+)
+
+func TestDurationInputMarshal(t *testing.T) {
+	type testStruct struct {
+		Value DurationInput
+	}
+
+	t1 := testStruct{Value: DurationInput(testDuration)}
+	res, err := json.Marshal(t1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Printf("%s\n", res)
+}
+
+func TestDurationInputNanoSeconds(t *testing.T) {
+	// Nano seconds to DurationInput
+	var ni DurationInput
+	ni = DurationInput(testDuration)
+	fmt.Printf("Duration ni in seconds is %d\n", ni.GetSeconds())
+	if testSeconds != ni.GetSeconds() {
+		t.Errorf("expected ni %d got %d", testSeconds, ni.GetSeconds())
+	}
+	fmt.Printf("Duration ni as a duration is %d\n", ni.GetDuration())
+	if testDuration != int64(ni.GetDuration()) {
+		t.Errorf("expected ni %d got %d", testDuration, ni.GetDuration())
+	}
+	fmt.Printf("Duration ni raw value is %d\n", ni.GetRawValue())
+	if testDuration != int64(ni.GetRawValue()) {
+		t.Errorf("expected ni %d got %d", testDuration, ni.GetRawValue())
+	}
+}
+
+func TestDurationInputSeconds(t *testing.T) {
+	// Seconds to DurationInput
+	var ti DurationInput
+	ti = DurationInput(testSeconds)
+	fmt.Printf("Duration ti in seconds is %d\n", ti.GetSeconds())
+	if testSeconds != ti.GetSeconds() {
+		t.Errorf("expected ti %d got %d", testSeconds, ti.GetSeconds())
+	}
+	fmt.Printf("Duration ti as a duration is %d\n", ti.GetDuration())
+	if testDuration != int64(ti.GetDuration()) {
+		t.Errorf("expected ni %d got %d", testDuration, ti.GetDuration())
+	}
+	fmt.Printf("Duration ti raw value is %d\n", ti.GetRawValue())
+	if testSeconds != int64(ti.GetRawValue()) {
+		t.Errorf("expected ni %d got %d", testDuration, ti.GetRawValue())
+	}
+}
