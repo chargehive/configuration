225da823277d3758503f5ce1f2c1b7daacf3cc8a
Luke Hines <lukehines@protonmail.com>
2020-03-23 14:03:25 +0000

    init on trigger

--- v1/scheduler/init.go
+++ v1/scheduler/init.go
@@ -11,6 +11,7 @@ func GetHandlers() []object.KindHandler {
 	funcs = append(funcs, sequentialScheduler()...)
 	funcs = append(funcs, onDemandScheduler()...)
 	funcs = append(funcs, refundScheduler()...)
+	funcs = append(funcs, onTriggerScheduler()...)
 	return funcs
 }
 
@@ -27,6 +28,14 @@ func sequentialScheduler() []object.KindHandler {
 	return []object.KindHandler{
 		object.NewKindHandler(o.GetKind(), object.KindHandlerDefaultVersion, func() object.Specification { return &Sequential{} }),
 		object.NewKindHandler(o.GetKind(), o.GetVersion(), func() object.Specification { return &Sequential{} }),
+	}
+}
+
+func onTriggerScheduler() []object.KindHandler {
+	o := OnTrigger{}
+	return []object.KindHandler{
+		object.NewKindHandler(o.GetKind(), object.KindHandlerDefaultVersion, func() object.Specification { return &OnTrigger{} }),
+		object.NewKindHandler(o.GetKind(), o.GetVersion(), func() object.Specification { return &OnTrigger{} }),
 	}
 }
 
