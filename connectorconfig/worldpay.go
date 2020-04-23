bdc929927572267eb3e079baac3671a7a9f96adb
Steve Barrow <steve@infurio.com>
2020-04-23 17:35:33 +0100

    rebased from master

--- connectorconfig/worldpay.go
+++ connectorconfig/worldpay.go
@@ -2,7 +2,6 @@ package connectorconfig
 
 import (
 	"encoding/json"
-
 	"github.com/chargehive/configuration/v1/connector"
 )
 
@@ -19,14 +18,34 @@ const (
 )
 
 type WorldpayCredentials struct {
-	Username              *string             `json:"username" yaml:"username" validate:"required,gt=0"`
-	Password              *string             `json:"password" yaml:"password" validate:"required,gt=0"`
-	MerchantID            string              `json:"merchantID" yaml:"merchantID" validate:"gte=1,lte=50"`
-	ReportGroup           string              `json:"reportGroup" yaml:"reportGroup" validate:"gte=1,lte=25"`
-	Environment           WorldpayEnvironment `json:"environment" yaml:"environment" validate:"oneof=sandbox postlive transactpostlive production productiontransact prelive transactprelive"`
-	CardinalApiIdentifier *string             `json:"cardinalApiIdentifier" yaml:"cardinalApiIdentifier" validate:"required"`
-	CardinalApiKey        *string             `json:"cardinalApiKey" yaml:"cardinalApiKey" validate:"required"`
-	CardinalOrgUnitId     *string             `json:"cardinalOrgUnitId" yaml:"cardinalOrgUnitId" validate:"required"`
+	Username                 *string             `json:"username" yaml:"username" validate:"required,gt=0"`
+	Password                 *string             `json:"password" yaml:"password" validate:"required,gt=0"`
+	MerchantID               string              `json:"merchantID" yaml:"merchantID" validate:"gte=1,lte=50"`
+	ReportGroup              string              `json:"reportGroup" yaml:"reportGroup" validate:"gte=1,lte=25"`
+	Environment              WorldpayEnvironment `json:"environment" yaml:"environment" validate:"oneof=sandbox postlive transactpostlive production productiontransact prelive transactprelive"`
+	CardinalApiIdentifier    *string             `json:"cardinalApiIdentifier" yaml:"cardinalApiIdentifier" validate:"required"`
+	CardinalApiKey           *string             `json:"cardinalApiKey" yaml:"cardinalApiKey" validate:"required"`
+	CardinalOrgUnitId        *string             `json:"cardinalOrgUnitId" yaml:"cardinalOrgUnitId" validate:"required"`
+	AppleMerchantIdentifier  string              `json:"appleMerchantIdentifier" yaml:"appleMerchantIdentifier"`
+	AppleMerchantDisplayName string              `json:"appleMerchantDisplayName" yaml:"appleMerchantDisplayName"`
+	AppleInitiative          string              `json:"appleInitiative" yaml:"appleInitiative" validate:"omitempty,oneof=web messaging"`
+	AppleInitiativeContext   string              `json:"appleInitiativeContext" yaml:"appleInitiativeContext"`
+	AppleMerchantCertificate *string             `json:"appleMerchantPublicKey" yaml:"appleMerchantPublicKey" validate:"required"`
+	AppleMerchantPrivateKey  *string             `json:"appleMerchantPrivateKey" yaml:"appleMerchantPrivateKey" validate:"required"`
+}
+
+func (c WorldpayCredentials) GetAppleMerchantPublicKey() string {
+	if c.AppleMerchantCertificate == nil {
+		return ""
+	}
+	return *c.AppleMerchantCertificate
+}
+
+func (c WorldpayCredentials) GetAppleMerchantPrivateKey() string {
+	if c.AppleMerchantPrivateKey == nil {
+		return ""
+	}
+	return *c.AppleMerchantPrivateKey
 }
 
 func (c WorldpayCredentials) GetCardinalApiIdentifier() string {
@@ -63,7 +82,7 @@ func (c *WorldpayCredentials) Validate() error {
 }
 
 func (c *WorldpayCredentials) GetSecureFields() []*string {
-	return []*string{c.Username, c.Password, c.CardinalApiIdentifier, c.CardinalApiKey}
+	return []*string{c.Username, c.Password, c.CardinalApiIdentifier, c.CardinalApiKey, c.AppleMerchantPrivateKey, c.AppleMerchantCertificate}
 }
 
 func (c *WorldpayCredentials) ToConnector() connector.Connector {
@@ -81,5 +100,13 @@ func (c WorldpayCredentials) SupportsSca() bool {
 }
 
 func (c WorldpayCredentials) SupportsApplePay() bool {
-	return true
+	if c.AppleMerchantIdentifier != "" &&
+		c.AppleMerchantDisplayName != "" &&
+		c.AppleInitiative != "" &&
+		c.AppleInitiativeContext != "" &&
+		(c.AppleMerchantCertificate != nil && c.AppleMerchantCertificate != new(string)) &&
+		(c.AppleMerchantPrivateKey != nil && c.AppleMerchantPrivateKey != new(string)) {
+		return true
+	}
+	return false
 }
