954a2de31c5f440f4e26165a671c4d1eb554f884
Steve Barrow <steve@infurio.com>
2020-07-01 15:40:19 +0100

    Removed static fields like API version and number Added comments to fields to show nesting and structure Moved fields around so that like components are contiguous Moved PayPageID from google struct into worldpay as its vendor specific

--- connectorconfig/worldpay.go
+++ connectorconfig/worldpay.go
@@ -27,6 +27,7 @@ type WorldpayCredentials struct {
 	CardinalApiIdentifier *string             `json:"cardinalApiIdentifier" yaml:"cardinalApiIdentifier" validate:"required"`
 	CardinalApiKey        *string             `json:"cardinalApiKey" yaml:"cardinalApiKey" validate:"required"`
 	CardinalOrgUnitId     *string             `json:"cardinalOrgUnitId" yaml:"cardinalOrgUnitId" validate:"required"`
+	GooglePayPageId       string              `json:"googlePayPageId"` // vantiv:merchantPayPageId
 	GooglePay
 	ApplePay
 }
