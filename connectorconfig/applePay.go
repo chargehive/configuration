40acb6d432de1f37b2f24f13e4c164b7b3c53ad8
Infurio <steve@infurio.com>
2020-07-14 09:44:49 +0100

    Improved Validation for Google/Apple Pay (#37)
    * Added better validation for googlepay
    Added constants for applepay reference
    
    * standardised formatting of googlepay and applepay from strings to structs with working validation

--- connectorconfig/applePay.go
+++ connectorconfig/applePay.go
@@ -1,8 +1,8 @@
 package connectorconfig
 
 type ApplePay struct {
-	// AppleMerchantIdentifier Merchant Identifier specified in the Apple Developer Merchant section
-	AppleMerchantIdentifier string `json:"appleMerchantIdentifier,omitempty" yaml:"appleMerchantIdentifier,omitempty"`
+	// AppleMerchantIdentifier REQUIRED TO ENABLE APPLE PAY Merchant Identifier specified in the Apple Developer Merchant section
+	AppleMerchantIdentifier string `json:"appleMerchantIdentifier,omitempty" yaml:"appleMerchantIdentifier,omitempty" validate:"-"`
 	// AppleMerchantDisplayName Value to be displayed on the payment page
 	AppleMerchantDisplayName string `json:"appleMerchantDisplayName,omitempty" yaml:"appleMerchantDisplayName,omitempty" validate:"required_with=AppleMerchantIdentifier"`
 	// AppleMerchantCertificate Merchant certificate in the Apple Developer Merchant section (must be base64 encoded!)
@@ -10,10 +10,38 @@ type ApplePay struct {
 	// AppleMerchantPrivateKey Merchant private key generated from the CSR in the Apple Developer Merchant section (must be base64 encoded!)
 	AppleMerchantPrivateKey *string `json:"appleMerchantPrivateKey" yaml:"appleMerchantPrivateKey" validate:"required"`
 	// AppleSupportedNetworks Specifies which card networks will be accepted by ApplePay
-	AppleSupportedNetworks []string `json:"appleSupportedNetworks" yaml:"appleSupportedNetworks" validate:"required_with=AppleMerchantIdentifier,dive,oneof=amex cartesBancaires chinaUnionPay discover eftpos electron elo interac jcb mada maestro masterCard privateLabel visa vPay"`
+	AppleSupportedNetworks []AppleSupportedNetwork `json:"appleSupportedNetworks,omitempty" yaml:"appleSupportedNetworks,omitempty" validate:"required_with=AppleMerchantIdentifier,dive,oneof=amex cartesBancaires chinaUnionPay discover eftpos electron elo interac jcb mada maestro masterCard privateLabel visa vPay"`
 	// AppleMerchantCapabilities The payment capabilities supported by the merchant
-	AppleMerchantCapabilities []string `json:"appleMerchantCapabilities" yaml:"appleMerchantCapabilities" validate:"required_with=AppleMerchantIdentifier,dive,oneof=supports3DS supportsCredit supportsDebit supportsEMV"`
+	AppleMerchantCapabilities []AppleMerchantCapability `json:"appleMerchantCapabilities,omitempty" yaml:"appleMerchantCapabilities,omitempty" validate:"required_with=AppleMerchantIdentifier,dive,oneof=supports3DS supportsCredit supportsDebit supportsEMV"`
 }
+
+type (
+	AppleMerchantCapability string
+	AppleSupportedNetwork   string
+)
+
+const (
+	AppleMerchantCapabilitysupports3DS    AppleMerchantCapability = "supports3DS"
+	AppleMerchantCapabilitysupportsCredit AppleMerchantCapability = "supportsCredit"
+	AppleMerchantCapabilitysupportsDebit  AppleMerchantCapability = "supportsDebit"
+	AppleMerchantCapabilitysupportsEMV    AppleMerchantCapability = "supportsEMV"
+
+	AppleSupportedNetworkAmex            AppleSupportedNetwork = "amex"
+	AppleSupportedNetworkCartesBancaires AppleSupportedNetwork = "cartesBancaires"
+	AppleSupportedNetworkChinaUnionPay   AppleSupportedNetwork = "chinaUnionPay"
+	AppleSupportedNetworkDiscover        AppleSupportedNetwork = "discover"
+	AppleSupportedNetworkEFTPos          AppleSupportedNetwork = "eftpos"
+	AppleSupportedNetworkElectron        AppleSupportedNetwork = "electron"
+	AppleSupportedNetworkELO             AppleSupportedNetwork = "elo"
+	AppleSupportedNetworkInterac         AppleSupportedNetwork = "interac"
+	AppleSupportedNetworkJCB             AppleSupportedNetwork = "jcb"
+	AppleSupportedNetworkMada            AppleSupportedNetwork = "mada"
+	AppleSupportedNetworkMaestro         AppleSupportedNetwork = "maestro"
+	AppleSupportedNetworkMasterCard      AppleSupportedNetwork = "masterCard"
+	AppleSupportedNetworkPrivateLabel    AppleSupportedNetwork = "privateLabel"
+	AppleSupportedNetworkVisa            AppleSupportedNetwork = "visa"
+	AppleSupportedNetworkVPay            AppleSupportedNetwork = "vPay"
+)
 
 func (c ApplePay) GetAppleMerchantPublicKey() string {
 	if c.AppleMerchantCertificate == nil {
