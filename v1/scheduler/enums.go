844451e640c5739edf9a061f48d172e4c6fce4c9
Brooke Bryan <brooke@bajb.net>
2020-10-16 10:22:08 +0100

    Charge Initialisation Time support (#59)
    * Charge Initialisation Time support
    
    * Updated comment

--- v1/scheduler/enums.go
+++ v1/scheduler/enums.go
@@ -118,4 +118,6 @@ const (
 	TimeZoneULT TimeZone = "ULT"
 	// TimeZoneUTC  Universal Time Coordinated
 	TimeZoneUTC TimeZone = "UTC"
+	// TimeZoneCIT Charge Initialisation Time (Midnight = Charge Time)
+	TimeZoneCIT TimeZone = "CIT"
 )
