faa2309b6f59a04e750f65002ae2e98a1aefe094
Brooke Bryan <brooke@bajb.net>
2022-04-21 09:41:57 +0100

    Store the config id instead of credential ID on legacy connector configurations

--- v1/connector/connector.go
+++ v1/connector/connector.go
@@ -27,8 +27,8 @@ type Connector struct {
 	ProcessingState ProcessingState `json:"processingState,omitempty" yaml:"processingState,omitempty"`
 	Library         string          `json:"library" yaml:"library" validate:"required,oneof=sandbox authorize braintree qualpay stripe paysafe worldpay paypal-websitepaymentspro paypal-expresscheckout vindicia chargehive maxmind cybersource paysafe-accountupdater bottomline checkout kount"`
 	Configuration   []byte          `json:"configuration" yaml:"configuration" validate:"required"`
-	CredentialID    string          `json:"credentialId,omitempty" yaml:"credentialId,omitempty"`
-	CredentialAuth  string          `json:"credentialAuth,omitempty" yaml:"credentialAuth,omitempty"`
+	ConfigID        string          `json:"configId,omitempty" yaml:"configId,omitempty"`
+	ConfigAuth      string          `json:"configAuth,omitempty" yaml:"configAuth,omitempty"`
 }
 
 // GetKind returns the Connector kind
