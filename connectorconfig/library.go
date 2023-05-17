0af577702428a790c1a875be63f6c5de00bca689
Tom Kay <oridan82@gmail.com>
2023-05-17 10:44:37 +0100

    add GP and AP to checkout SupportsMethod (#130)

--- connectorconfig/library.go
+++ connectorconfig/library.go
@@ -212,7 +212,9 @@ func (l Library) SupportsMethod(methodType chtype.PaymentMethodType, methodProvi
 	case LibraryBottomline:
 		return methodType == chtype.PAYMENT_METHOD_TYPE_DIRECTDEBIT
 	case LibraryCheckout:
-		return methodType == chtype.PAYMENT_METHOD_TYPE_CARD
+		return methodType == chtype.PAYMENT_METHOD_TYPE_CARD ||
+			(methodType == chtype.PAYMENT_METHOD_TYPE_DIGITALWALLET && methodProvider == chtype.PAYMENT_METHOD_PROVIDER_APPLEPAY) ||
+			(methodType == chtype.PAYMENT_METHOD_TYPE_DIGITALWALLET && methodProvider == chtype.PAYMENT_METHOD_PROVIDER_GOOGLEPAY)
 	case LibraryChargeHive:
 		return methodType == chtype.PAYMENT_METHOD_TYPE_CARD
 	case LibraryMaxMind:
