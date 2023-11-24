39278db4eca953c85ac07cb97b52d0bfa8a3efd1
Okechukwu Ugwu <fobilow@gmail.com>
2023-11-24 10:22:00 +0100

    Add InovioPay config (#160)
    * DEV-546: Add support for InovioPay
    
    * InovioPay - implement generate logic
    
    * make inoviopay a valid connector library

---
+++ connectorconfig/inoviopay.go
@@ -0,0 +1,75 @@
+package connectorconfig
+
+import (
+	"encoding/json"
+
+	"github.com/chargehive/configuration/environment"
+	"github.com/chargehive/configuration/v1/connector"
+	"github.com/chargehive/proto/golang/chargehive/chtype"
+)
+
+type InovioPayEnvironment string
+
+const (
+	InovioPayEnvironmentSandbox    InovioPayEnvironment = "sandbox"
+	InovioPayEnvironmentProduction InovioPayEnvironment = "production"
+)
+
+type InovioPayCredentials struct {
+	Username    *string              `json:"username" yaml:"username" validate:"required,gt=0"`
+	Password    *string              `json:"password" yaml:"password" validate:"required,gt=0"`
+	SiteID      string               `json:"siteId" yaml:"siteId" validate:"required,gt=0"`
+	Environment InovioPayEnvironment `json:"environment" yaml:"environment" validate:"oneof=sandbox production"`
+}
+
+func (c *InovioPayCredentials) GetMID() string {
+	return c.SiteID
+}
+
+func (c *InovioPayCredentials) GetLibrary() Library {
+	return LibraryInovioPay
+}
+
+func (c *InovioPayCredentials) GetSupportedTypes() []LibraryType {
+	return []LibraryType{LibraryTypePayment}
+}
+
+func (c *InovioPayCredentials) Validate() error {
+	return nil
+}
+
+func (c *InovioPayCredentials) GetSecureFields() []*string {
+	return []*string{c.Username, c.Password}
+}
+
+func (c *InovioPayCredentials) ToConnector() connector.Connector {
+	con := connector.Connector{Library: string(c.GetLibrary())}
+	con.Configuration, _ = json.Marshal(c)
+	return con
+}
+
+func (c *InovioPayCredentials) FromJson(input []byte) error {
+	return json.Unmarshal(input, c)
+}
+
+func (c *InovioPayCredentials) SupportsSca() bool {
+	return true
+}
+
+func (c *InovioPayCredentials) SupportsMethod(methodType chtype.PaymentMethodType, methodProvider chtype.PaymentMethodProvider) bool {
+	if !c.GetLibrary().SupportsMethod(methodType, methodProvider) {
+		return false
+	}
+	return true
+}
+
+func (c *InovioPayCredentials) CanPlanModeUse(mode environment.Mode) bool {
+	if mode == environment.ModeSandbox && c.Environment == InovioPayEnvironmentProduction {
+		return false
+	}
+	return true
+}
+
+func (c *InovioPayCredentials) IsRecoveryAgent() bool {
+	return false
+}
