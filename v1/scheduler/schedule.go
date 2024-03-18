8c74c4c6b6e42fb59a9ea2e22b3af1c7cbb98c96
Brooke Bryan <brooke@bajb.net>
2024-03-18 12:32:26 +0000

    Time window capacity

--- v1/scheduler/schedule.go
+++ v1/scheduler/schedule.go
@@ -19,6 +19,9 @@ type Schedule struct {
 	// TimeSyncHour is an hour designation (0-23) i.e 2 == 2AM. Ignored if TimeDelaySync is set to None
 	TimeSyncHour int `json:"timeSyncHour" yaml:"timeSyncHour" validate:"min=0,max=23"`
 
+	// TimeWindowHours is the number of hours grace for a transaction to be processed within.  Allowing for a transaction to be processed within X hours of the requested delay & sync hour e.g. 10am > 11am with a TimeWindowHours of 1
+	TimeWindowHours int `json:"timeWindowHours" yaml:"timeWindowHours" validate:"gte=0"`
+
 	// TimeSyncZone indicates the timezone that the TimeSyncHour is relative to. Ignored if TimeDelaySync is set to None
 	TimeSyncZone TimeZone `json:"timeSyncZone" yaml:"timeSyncZone" validate:"oneof=ULT UTC CIT"`
 }
