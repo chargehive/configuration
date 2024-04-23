eba009d93c2a4acdf2b6ff9b1e2aab694f335e00
Tom Kay <oridan82@gmail.com>
2024-04-23 19:00:34 +0100

    add stickyio scheduler connector

---
+++ connectorconfig/sticky-io.go
@@ -0,0 +1,78 @@
+package connectorconfig
+
+import (
+	"encoding/json"
+
+	"github.com/chargehive/configuration/environment"
+	"github.com/chargehive/configuration/v1/connector"
+	"github.com/chargehive/proto/golang/chargehive/chtype"
+)
+
+type StickyIOCredentials struct {
+	ApiKey             *string                   `json:"apiKey" yaml:"apiKey" validate:"required,gt=0"`
+	MerchantName       string                    `json:"merchantName" yaml:"merchantName"`
+	MCC                string                    `json:"mcc" yaml:"mcc"`
+	AcquirerBIN        string                    `json:"acquirerBIN" yaml:"acquirerBIN"`
+	AcquirerMerchantID string                    `json:"acquirerMerchantID" yaml:"acquirerMerchantID"`
+	Environment        ThreeDSecureIOEnvironment `json:"environment" yaml:"environment" validate:"oneof=sandbox production"`
+}
+
+func (c *StickyIOCredentials) GetMID() string {
+	return c.MerchantName
+}
+
+func (c *StickyIOCredentials) GetApiKey() string {
+	if c.ApiKey == nil {
+		return ""
+	}
+	return *c.ApiKey
+}
+
+func (c *StickyIOCredentials) GetLibrary() Library {
+	return LibraryStickyIO
+}
+
+func (c *StickyIOCredentials) GetSupportedTypes() []LibraryType {
+	return []LibraryType{LibraryTypeScheduler}
+}
+
+func (c *StickyIOCredentials) Validate() error {
+	return nil
+}
+
+func (c *StickyIOCredentials) GetSecureFields() []*string {
+	return []*string{c.ApiKey}
+}
+
+func (c *StickyIOCredentials) ToConnector() connector.Connector {
+	con := connector.Connector{Library: string(c.GetLibrary())}
+	con.Configuration, _ = json.Marshal(c)
+	return con
+}
+
+func (c *StickyIOCredentials) FromJson(input []byte) error {
+	return json.Unmarshal(input, c)
+}
+
+func (c *StickyIOCredentials) SupportsSca() bool {
+	return true
+}
+
+func (c *StickyIOCredentials) SupportsMethod(methodType chtype.PaymentMethodType, methodProvider chtype.PaymentMethodProvider) bool {
+	if !c.GetLibrary().SupportsMethod(methodType, methodProvider) {
+		return false
+	}
+	return true
+}
+
+func (c *StickyIOCredentials) SupportsCountry(country string) bool {
+	return true
+}
+
+func (c *StickyIOCredentials) CanPlanModeUse(mode environment.Mode) bool {
+	return true
+}
+
+func (c *StickyIOCredentials) IsRecoveryAgent() bool {
+	return false
+}
