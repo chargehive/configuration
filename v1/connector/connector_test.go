e125b446fc760db82c2534b4c78fab0eb4574e14
Oke Ugwu <fobilow@gmail.com>
2024-07-11 13:42:15 +0100

    refactor test

--- v1/connector/connector_test.go
+++ v1/connector/connector_test.go
@@ -9,46 +9,31 @@ import (
 func TestConnectorValidate(t *testing.T) {
 	validate := validator.New()
 
+	con := &Connector{
+		ProcessingState: ProcessingStateLive,
+		Library:         "",
+		Configuration:   []byte("xxxx"),
+		ConfigID:        "",
+		ConfigAuth:      "",
+		EnablePCIB:      false,
+		SCAConnectorID:  "",
+	}
+
 	t.Run("empty library", func(t *testing.T) {
-		con := &Connector{
-			ProcessingState: ProcessingStateLive,
-			Library:         "",
-			Configuration:   []byte("xxxx"),
-			ConfigID:        "",
-			ConfigAuth:      "",
-			EnablePCIB:      false,
-			SCAConnectorID:  "",
-		}
 		if err := validate.Struct(con); err != nil {
 			t.Error(err)
 		}
 	})
 
 	t.Run("invalid library", func(t *testing.T) {
-		con := &Connector{
-			ProcessingState: ProcessingStateLive,
-			Library:         "chargehive",
-			Configuration:   []byte("xxxx"),
-			ConfigID:        "",
-			ConfigAuth:      "",
-			EnablePCIB:      false,
-			SCAConnectorID:  "",
-		}
+		con.Library = "chargehive"
 		if err := validate.Struct(con); err == nil {
 			t.Error("expected an error")
 		}
 	})
 
 	t.Run("valid library", func(t *testing.T) {
-		con := &Connector{
-			ProcessingState: ProcessingStateLive,
-			Library:         "checkout",
-			Configuration:   []byte("xxxx"),
-			ConfigID:        "",
-			ConfigAuth:      "",
-			EnablePCIB:      false,
-			SCAConnectorID:  "",
-		}
+		con.Library = "checkout"
 		if err := validate.Struct(con); err != nil {
 			t.Error(err)
 		}
