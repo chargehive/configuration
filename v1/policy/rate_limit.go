8335fed6b07ddae5905445eb4e53eb521d623fde
Tom Kay <oridan82@gmail.com>
2024-12-12 16:23:16 +0000

    add rate limit policy

---
+++ v1/policy/rate_limit.go
@@ -0,0 +1,33 @@
+package policy
+
+import (
+	"github.com/chargehive/configuration/object"
+)
+
+// KindPolicyRateLimit is the identifier for a RateLimitPolicy config
+const KindPolicyRateLimit object.Kind = "RateLimitPolicy"
+
+type RateLimitKey string
+
+const (
+	RateLimitKeyChargePlacementID RateLimitKey = "PlacementID"
+	RateLimitKeyMerchantReference RateLimitKey = "MerchantReference"
+	RateLimitKeyCorrelationID     RateLimitKey = "CorrelationID"
+	RateLimitKeyBillingProfileID  RateLimitKey = "BillingProfileID"
+	RateLimitKeyIP                RateLimitKey = "IP"
+	RateLimitKeyUserAgent         RateLimitKey = "UserAgent"
+	RateLimitKeyPlatform          RateLimitKey = "Platform"
+)
+
+type RateLimitPolicy struct {
+	// LimitProperty is the property to limit the rate by, global if empty
+	LimitProperty RateLimitKey `json:"limitProperty" yaml:"limitProperty" validate:"oneof=PlacementID MerchantReference CorrelationID BillingProfileID IP UserAgent Platform"`
+	// HardLimit is the maximum number of requests allowed in the window
+	HardLimit int32 `json:"hardLimit" yaml:"hardLimit" validate:"required,min=1"`
+	// Window is the time window in minutes that the limit is applied to
+	Window int32 `json:"window" yaml:"window" validate:"required,min=1"`
+}
+
+func (r RateLimitPolicy) GetKind() object.Kind { return KindPolicyRateLimit }
+
+func (r RateLimitPolicy) GetVersion() string { return "v1" }
