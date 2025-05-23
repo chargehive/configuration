6130483e5f8abc5b7601cd3531973aa44a14da0f
Tom Kay <oridan82@gmail.com>
2025-05-23 13:07:00 +0100

    use "cht" style

--- v1/scheduler/attempt_config.go
+++ v1/scheduler/attempt_config.go
@@ -34,7 +34,7 @@ type AttemptConfig struct {
 	// Deprecated - use PreferredTokens
 	PreferNetworkToken bool `json:"preferNetworkToken,omitempty" yaml:"preferNetworkToken,omitempty"`
 
-	PreferredTokens []TokenSource `json:"preferredTokens,omitempty" yaml:"preferredTokens,omitempty" validate:"dive,lowercase,oneof=pan connector network-token google-pay apple-pay samsung-pay amazon-pay revolut-pay wechat-pay alipay paypal text-to-pay bacs sepa ach bank-transfer bancontact eps ideal przelewy-24 twint sofort multibanco klarna after-pay coinbase google-play apple-store"`
+	PreferredTokens []TokenSource `json:"preferredTokens,omitempty" yaml:"preferredTokens,omitempty" validate:"dive,lowercase,oneof=TS_PAN TS_CONNECTOR TS_NETWORK_TOKEN TS_GOOGLE_PAY TS_APPLE_PAY TS_SAMSUNG_PAY TS_AMAZON_PAY TS_REVOLUT_PAY TS_WECHAT_PAY TS_ALIPAY TS_PAYPAL TS_TEXT_TO_PAY TS_BACS TS_SEPA TS_ACH TS_BANK_TRANSFER TS_BANCONTACT TS_EPS TS_IDEAL TS_PRZELEWY24 TS_TWINT TS_SOFORT TS_MULTIBANCO TS_KLARNA TS_AFTER_PAY TS_COINBASE TS_GOOGLE_PLAY TS_APPLE_STORE"`
 
 	ShouldTokenize bool `json:"shouldTokenize,omitempty" yaml:"shouldTokenize,omitempty"`
 
