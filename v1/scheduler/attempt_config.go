9f8a88bed625a6798976d13f2ff958350bf97191
Tom Kay <oridan82@gmail.com>
2025-08-22 16:41:46 +0100

    RequireNetworkToken

--- v1/scheduler/attempt_config.go
+++ v1/scheduler/attempt_config.go
@@ -33,6 +33,9 @@ type AttemptConfig struct {
 	// PreferNetworkToken indicates if the network token should be used instead of the PAN
 	// Deprecated - use PreferredTokens
 	PreferNetworkToken bool `json:"preferNetworkToken,omitempty" yaml:"preferNetworkToken,omitempty"`
+	// RequireNetworkToken indicates if the network token should be used instead of the PAN
+	// Deprecated - use PreferredTokens
+	RequireNetworkToken bool `json:"requireNetworkToken,omitempty" yaml:"requireNetworkToken,omitempty"`
 
 	PreferredTokens []TokenSource `json:"preferredTokens,omitempty" yaml:"preferredTokens,omitempty" validate:"dive,uppercase,oneof=TS_PAN TS_CONNECTOR TS_NETWORK_TOKEN TS_GOOGLE_PAY TS_APPLE_PAY TS_SAMSUNG_PAY TS_AMAZON_PAY TS_REVOLUT_PAY TS_WECHAT_PAY TS_ALIPAY TS_PAYPAL TS_TEXT_TO_PAY TS_BACS TS_SEPA TS_ACH TS_BANK_TRANSFER TS_BANCONTACT TS_EPS TS_IDEAL TS_PRZELEWY24 TS_TWINT TS_SOFORT TS_MULTIBANCO TS_KLARNA TS_AFTER_PAY TS_COINBASE TS_GOOGLE_PLAY TS_APPLE_STORE"`
 
