362223d08e74fed1c0ff69bfa39508bcec1495c5
Adam Martin <adam.martin@chargehive.com>
2026-03-11 16:37:46 +0000

    add supported token types for apple store credentials

--- connectorconfig/applestore.go
+++ connectorconfig/applestore.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/chargehive/configuration/environment"
 	"github.com/chargehive/configuration/v1/connector"
+	"github.com/chargehive/configuration/v1/scheduler"
 	"github.com/chargehive/proto/golang/chargehive/chtype"
 )
 
@@ -69,4 +70,8 @@ func (c *AppleStoreCredentials) Supports3RI() bool {
 
 func (c *AppleStoreCredentials) IsAccountUpdater() bool {
 	return false
+}
+
+func (c *AppleStoreCredentials) SupportedTokenTypes() []scheduler.TokenSource {
+	return []scheduler.TokenSource{}
 }
